93730f6c83c475ea8fe54f5f3cea5103d467b4bd
Suhail89589 <mohdsuhail8958911@gmail.com>
2026-02-21 23:26:29 +0530

    Initialized Result and Dashboard with React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 
-// We create a separate Home component to keep App.jsx clean
+
 const Home = () => {
   return (
     <>
@@ -19,18 +19,18 @@ const Home = () => {
 const App = () => {
   return (
     <Router>
-      {/* Navbar stays here so it shows up on EVERY page */}
+      
       <Navbar />
 
       <Routes>
-        {/* Route for the Main Landing Page */}
+        
         <Route path="/" element={<Home />} />
 
-        {/* Route for the Login Page */}
+        
         <Route path="/login" element={<Login />} />
       </Routes>
 
-      {/* Footer stays here so it shows up on EVERY page */}
+      
       <Footer />
     </Router>
   );
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <div className="profile">
+        <div className="profile-card">
+          <img src="" alt="" />
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
